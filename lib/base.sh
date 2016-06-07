0b2c4f44e557824a487e7f4283626b8d7108122f
Elifarley C <elifarley@users.noreply.github.com>
2016-06-07 11:45:13 -0300

    Update base.sh

--- lib/base.sh
+++ lib/base.sh
@@ -25,27 +25,6 @@ mkrandom() { base64 -w0 /dev/urandom | tr -d "/+${2:-0Oo}" | dd bs="${1:-8}" cou
 mkrandomL() { mkrandom "$@" | tr '[[:upper:]]' '[[:lower:]]' ;}
 mkrandomU() { mkrandom "$@" | tr '[[:lower:]]' '[[:upper:]]' ;}
 
-strsplit() { (( $# == 3 )) || { echo "Usage: $FUNCNAME <str> <separator> <outvarname>"; return 1 ;}
-  IFS="$2" read -a "$3" <<< "$1"
-}
-
-# Disable file globbing; coalesce inner whitespace;
-# trim leading and trailing whitespace
-trim() { (set -f; echo $@) ;}
-strcontains() { test -z "${1##*$2*}" ; }; export -f strcontains
-strendswith() { test ! "${1%%*$2}"; }
-strstartswith() { test ! "${1##$2*}"; }
-# See http://mywiki.wooledge.org/BashPitfalls#if_.5B.5B_.24foo_.3D.2BAH4_.27some_RE.27_.5D.5D
-contains_asterisk() { local ASTERISK='\*'; [[ $1 =~ $ASTERISK ]] ;}
-
-escape_quotes() {
-  result=''
-  for i in "$@"; do
-    result="$result \"${i//\"/\\\"}\""
-  done;
-  echo $result
-}
-
 exec_at_dir() { bash -c 'cd "$1" && shift && "$@"' exec-at-dir "$@"; }
 
 wait_file() {
