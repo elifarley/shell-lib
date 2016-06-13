7f96a218095005257ee6b5b8d2809459f92d8c8b
Elifarley C <elifarley@users.noreply.github.com>
2016-06-13 10:22:58 -0300

    New function: "os_version"

--- lib/base.sh
+++ lib/base.sh
@@ -32,3 +32,10 @@ typeof() {
   echo $result
 
 }
+
+os_version() { (
+  test -f /etc/os-release && . /etc/os-release
+  local VERSION="$VERSION_ID"
+  test -f /etc/debian_version && VERSION="$(cat /etc/debian_version)"
+  echo "$PRETTY_NAME [$VERSION]"
+) }
