525909050b69bd020d620d0834aabe5f892c2d5a
Elifarley C <elifarley@users.noreply.github.com>
2016-12-09 14:51:08 -0200

    Use defaults

--- lib/container-img-naming.sh
+++ lib/container-img-naming.sh
@@ -16,10 +16,10 @@ set_img_vars() {
     return 1
   }
 
-  local IMG_REPO="$(echo $1 | tr '[:upper:]' '[:lower:]')"; shift
-  local IMG_JOB_NAME="$1"; test $# -gt 0 && shift
-  local IMG_BUILD_NUMBER="${1:-$(userAtHost)}"; test $# -gt 0 && shift
-  local CHANGESET="${1:-$(chageset)}"
+  local IMG_REPO="$(echo ${1:-$IMG_REPO} | tr '[:upper:]' '[:lower:]')"; shift
+  local IMG_JOB_NAME="${1:-$JOB_NAME}"; test $# -gt 0 && shift
+  local IMG_BUILD_NUMBER="${1:-${BUILD_NUMBER:-$(userAtHost)}}"; test $# -gt 0 && shift
+  local CHANGESET="${1:-${GIT_COMMIT:-$(chageset)}}"
   
   IMG_PREFIX_BASE="$(echo $IMG_REPO:$IMG_JOB_NAME | tr '[:upper:]' '[:lower:]')."
   IMG_PREFIX_BN="$(echo ${IMG_PREFIX_BASE}$IMG_BUILD_NUMBER | tr '[:upper:]' '[:lower:]')"
