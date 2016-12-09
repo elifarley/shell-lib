96e977fdc190e9bf819d8f2b51e49a653b43c7c7
Elifarley C <elifarley@users.noreply.github.com>
2016-12-09 13:58:40 -0200

    Update container-img-naming.sh

--- lib/container-img-naming.sh
+++ lib/container-img-naming.sh
@@ -10,6 +10,8 @@ changeset_short() {
   printf '%.7s' "${1:-$(chageset)}"
 }
 
-IMG_PREFIX_BASE="$(echo $IMG_REPO:$IMG_JOB_NAME | tr '[:upper:]' '[:lower:]')."
-IMG_PREFIX_BN="$(echo ${IMG_PREFIX_BASE}$IMG_BUILD_NUMBER | tr '[:upper:]' '[:lower:]')"
-IMG_PREFIX_CS="$REPO_PREFIX_BN-$(changeset_short "$CHANGESET")"
+set_img_vars() {
+  IMG_PREFIX_BASE="$(echo $IMG_REPO:$IMG_JOB_NAME | tr '[:upper:]' '[:lower:]')."
+  IMG_PREFIX_BN="$(echo ${IMG_PREFIX_BASE}$IMG_BUILD_NUMBER | tr '[:upper:]' '[:lower:]')"
+  IMG_PREFIX_CS="$REPO_PREFIX_BN-$(changeset_short "$CHANGESET")"
+}
