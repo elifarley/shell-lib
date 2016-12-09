d2474093b2c8c238fc375c0b66f2a09516d91c0d
Elifarley C <elifarley@users.noreply.github.com>
2016-12-09 14:20:01 -0200

    Update container-img-naming.sh

--- lib/container-img-naming.sh
+++ lib/container-img-naming.sh
@@ -11,6 +11,11 @@ changeset_short() {
 }
 
 set_img_vars() {
+  test $# -ge 2 || {
+    printf 'Parameters:\n\nIMG_REPO\nUJOB_NAME\n<BUILD_NUMBER>\n<CHANGESET>'
+    return 1
+  }
+
   local IMG_REPO="$(echo $1 | tr '[:upper:]' '[:lower:]')"; shift
   local IMG_JOB_NAME="$1"; test $# -gt 0 && shift
   local IMG_BUILD_NUMBER="${1:-$(userAtHost)}"; test $# -gt 0 && shift
