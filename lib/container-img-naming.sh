0d9575ab9bb6dcc5a0975e70c49ce46c532cb150
elifarley <elifarley@gmail.com>
2017-01-24 16:18:54 -0200

    set_img_vars: use new function "getprop_container"

--- lib/container-img-naming.sh
+++ lib/container-img-naming.sh
@@ -2,18 +2,20 @@ userAtHost() {
   echo $(id -nu)-at-$(hostname -s)
 }
 
+getprop_container() {
+  local cprops="$(find .. . -maxdepth 1 -name container.properties)"
+  test "$cprops" -a -s "$cprops" && \
+    getprop "$cprops" "$1"
+}
+
 set_img_vars() {
   test $# -ge 2 || {
-    printf 'Parameters:\n\nIMG_REPO\nJOB_NAME\n<BUILD_NUMBER>\n<CHANGESET>'
+    printf 'Parameters:\n\nIMG_REPO\nIMG_NAME\n<BUILD_NUMBER>\n<CHANGESET>'
     return 1
   }
 
   local IMG_REPO="$(echo ${1:-$IMG_REPO})"; test $# -gt 0 && shift
-  test "$IMG_REPO" || {
-    local cprops="$(find .. . -maxdepth 1 -name container.properties)"
-    test "$cprops" -a -s "$cprops" && \
-    IMG_REPO="$(getprop "$cprops" IMG_REPO)"
-  }
+  IMG_REPO="${IMG_REPO:-$(getprop_container IMG_REPO)}"
   IMG_REPO="$(echo $IMG_REPO | tr ' ' '-')"
   test "$IMG_REPO" || {
     cat <<-EOF
@@ -23,11 +25,11 @@ EOF
  return 0
   }
 
-  local IMG_JOB_NAME="$(echo "${1:-$JOB_NAME}" | tr '/ ' '.-')"; test $# -gt 0 && shift
+  local IMG_NAME="$(echo "${1:-$JOB_NAME}" | tr '/ ' '.-')"; test $# -gt 0 && shift
   local IMG_BUILD_NUMBER="$(echo ${1:-${BUILD_NUMBER:-$(userAtHost)}} | tr '/ ' '.-' )"; test $# -gt 0 && shift
   local CHANGESET="${1:-$(chageset)}"
 
-  IMG_PREFIX_BASE="$(echo $IMG_REPO:$IMG_JOB_NAME | tr '[:upper:]' '[:lower:]')."
+  IMG_PREFIX_BASE="$(echo $IMG_REPO:$IMG_NAME | tr '[:upper:]' '[:lower:]')."
   IMG_PREFIX_BN="$(echo ${IMG_PREFIX_BASE}$IMG_BUILD_NUMBER | tr '[:upper:]' '[:lower:]')"
   IMG_PREFIX_CS="$IMG_PREFIX_BN-$(changeset_short "$CHANGESET")"
 }
