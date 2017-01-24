5d26279fa5ed48171e99ef8d65badcda9f4f1630
Elifarley C <elifarley@users.noreply.github.com>
2017-01-24 10:17:07 -0200

    Simplification

--- lib/container-img-naming.sh
+++ lib/container-img-naming.sh
@@ -19,7 +19,7 @@ set_img_vars() {
   local IMG_REPO="$(echo ${1:-$IMG_REPO} | tr '[:upper:]' '[:lower:]')"; test $# -gt 0 && shift
   local IMG_JOB_NAME="$(echo "${1:-$JOB_NAME}" | tr '/' '.')"; test $# -gt 0 && shift
   local IMG_BUILD_NUMBER="${1:-${BUILD_NUMBER:-$(userAtHost)}}"; test $# -gt 0 && shift
-  local CHANGESET="${1:-${GIT_COMMIT:-$(chageset)}}"
+  local CHANGESET="${1:-$(chageset)}"
   
   IMG_PREFIX_BASE="$(echo $IMG_REPO:$IMG_JOB_NAME | tr '[:upper:]' '[:lower:]')."
   IMG_PREFIX_BN="$(echo ${IMG_PREFIX_BASE}$IMG_BUILD_NUMBER | tr '[:upper:]' '[:lower:]')"
