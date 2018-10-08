63eb11b042b46d84993390e97adfb3dd89059aef
Elifarley C <elifarley@users.noreply.github.com>
2018-10-08 09:23:53 -0300

    vcs.sh: Function 'changeset' now supports hg

--- lib/vcs.sh
+++ lib/vcs.sh
@@ -1,5 +1,10 @@
 changeset() {
-  echo "${GIT_COMMIT:-$(if test -d "$1"; then cd "$1"; fi; git 2>/dev/null rev-parse HEAD)}"
+  echo "${GIT_COMMIT:-${MERCURIAL_REVISION:-$(
+    if test -d "$1"; then cd "$1"; fi
+    test -d .git && git 2>/dev/null rev-parse HEAD \
+    || \
+    test -d .hg && hg log -r . --template '{node}\n'
+  )}}"
 }
 
 changeset_short() {
