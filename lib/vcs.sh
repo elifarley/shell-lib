8859958efad251a887ff5aa8a072f15f39f96168
Elifarley C <elifarley@users.noreply.github.com>
2018-10-08 09:36:25 -0300

    vcs.sh: New function 'vcs_url'

--- lib/vcs.sh
+++ lib/vcs.sh
@@ -7,6 +7,15 @@ changeset() {
   )}}"
 }
 
+vcs_url() {
+  echo "${GIT_URL:-$(
+    if test -d "$1"; then cd "$1"; fi
+    test -d .git && git 2>/dev/null remote get-url origin \
+    || \
+    test -d .hg && echo $(hg paths | head -n1 | cut -d= -f2)
+  )}"
+}
+
 changeset_short() {
   printf '%.7s' "${1:-$(changeset)}"
 }
