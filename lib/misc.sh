fd2efbf64bf44a4c717acd0335e5b215b849338b
Elifarley C <elifarley@users.noreply.github.com>
2020-07-22 12:56:31 +0200

    wait_file: test file existence using "-e"
    instead of "-f"

--- lib/misc.sh
+++ lib/misc.sh
@@ -49,7 +49,7 @@ wait_file() {
   local file="$1"; shift
   local wait_seconds="${1:-10}"; shift # 10 seconds as default timeout
 
-  until test $((wait_seconds--)) -eq 0 -o -f "$file" ; do sleep 1; done
+  until test $((wait_seconds--)) -eq 0 -o -e "$file" ; do sleep 1; done
 
   ((++wait_seconds))
 }
