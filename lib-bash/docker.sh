858a7ca7e550f49590815041c93f3d2921f67d62
Elifarley C <elifarley@users.noreply.github.com>
2020-07-26 14:23:50 +0200

    egrep -> grep -E

--- lib-bash/docker.sh
+++ lib-bash/docker.sh
@@ -2,7 +2,7 @@ docker_image_exists() {
   local image_full_name="$1"; shift
   local wait_time="${1:-5}"
   local search_term='Pulling|is up to date|not found'
-  local result="$((timeout --preserve-status "$wait_time" docker 2>&1 pull "$image_full_name" &) | grep -v 'Pulling repository' | egrep -o "$search_term")"
+  local result="$((timeout --preserve-status "$wait_time" docker 2>&1 pull "$image_full_name" &) | grep -v 'Pulling repository' | grep -E -o "$search_term")"
   test "$result" || { echo "Timed out too soon. Try using a wait_time greater than $wait_time..."; return 1 ;}
   echo $result | grep -vq 'not found'
 }
