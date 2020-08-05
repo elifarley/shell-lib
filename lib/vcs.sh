809d317a155166b83c9417c40f428c91155ea55e
Elifarley C <elifarley@users.noreply.github.com>
2020-08-05 09:13:52 +0200

    Improvements in changeset, vcs_url and changeset_short

--- lib/vcs.sh
+++ lib/vcs.sh
@@ -1,21 +1,21 @@
 changeset() {
   echo "${GIT_COMMIT:-${MERCURIAL_REVISION:-$(
-    if test -d "$1"; then cd "$1"; fi
+    test -d "$1" && cd "$1"
     test -d .git && git 2>/dev/null rev-parse HEAD \
-    || \
+      && return
     test -d .hg && hg log -r . --template '{node}\n'
   )}}"
 }
 
 vcs_url() {
   echo "${GIT_URL:-$(
-    if test -d "$1"; then cd "$1"; fi
+    test -d "$1" && cd "$1"
     test -d .git && git 2>/dev/null remote get-url origin \
-    || \
-    test -d .hg && echo $(hg paths | head -n1 | cut -d= -f2)
+      && return
+    test -d .hg && echo "$(hg paths | head -n1 | cut -d= -f2)"
   )}"
 }
 
 changeset_short() {
-  printf '%.7s' "${1:-$(changeset)}"
+  printf '%.7s' "${1:-"$(changeset)"}"
 }
