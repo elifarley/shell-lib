f89dec5c08390881983a63ec8d65395296f539f0
Elifarley C <elifarley@users.noreply.github.com>
2021-11-08 13:26:50 +0100

    Return if sourced

--- bin/template.sh
+++ bin/template.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+test "$-" && echo "Not meant to be sourced" && return # if called via '.'
 CMD_BASE="$(readlink -f "$0" 2>/dev/null || greadlink -f "$0")" || CMD_BASE="$0"; CMD_BASE="$(dirname "$CMD_BASE")"
 
 # See (maybe) https://stackoverflow.com/a/23011530/299109
