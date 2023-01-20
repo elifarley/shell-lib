553bcb170fc35914a1b9c4eb7d3f04d05292586a
Elifarley C <elifarley@users.noreply.github.com>
2023-01-20 11:13:33 +0100

    Added reference

--- lib-bash/str.sh
+++ lib-bash/str.sh
@@ -2,4 +2,5 @@ strsplit() { (( $# == 3 )) || { echo "Usage: $FUNCNAME <str> <separator> <outvar
   IFS="$2" read -a "$3" <<< "$1"
 }
 
+# See https://wiki.bash-hackers.org/syntax/pe#case_modification
 titleCase() { set ${*,,}; echo ${*^} ;}
