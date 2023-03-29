513641f367a7dfc97cac87c719bb8176197bd131
Elifarley C <519940+elifarley@users.noreply.github.com>
2023-03-29 21:08:51 +0200

    line_prefix could be the whole key name, so let's use an asterisk

--- lib/yaml.sh
+++ lib/yaml.sh
@@ -1,4 +1,4 @@
 yaml_lines() {
   local root_key="${1:-\S+}" line_prefix="${2:-[a-zA-Z]*}"
-  sed -En '/^'"$root_key"':/{:loop n; s!^\s*'"$line_prefix"'[^:]+:[ "]*([^"]+)"!\1!p; /^\S+:/q; b loop}'
+  sed -En '/^'"$root_key"':/{:loop n; s!^\s*'"$line_prefix"'[^:]*:[ "]*([^"]+)"!\1!p; /^\S+:/q; b loop}'
 }
