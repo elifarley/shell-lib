19be98ea4b42cdd579e0d2f95a2ea5a7a0822f23
Elifarley C <519940+elifarley@users.noreply.github.com>
2023-04-26 09:08:29 +0200

    Update chrome.sh

--- lib/chrome.sh
+++ lib/chrome.sh
@@ -1,7 +1,7 @@
 # See https://chromedriver.chromium.org/downloads/version-selection
 chromedriver_install() (
   target="${1:-/usr/local/bin}"
-  chrome_version=$(chrome --version | tr -c -d '0-9.')
+  chrome_version=$(google-chrome --version | tr -c -d '0-9.')
   curl -LSs https://chromedriver.storage.googleapis.com/LATEST_RELEASE_"${chrome_version%.*}" \
   | xargs -Ichromedriver-version \
     curl -LSs https://chromedriver.storage.googleapis.com/chromedriver-version/chromedriver_linux64.zip \
