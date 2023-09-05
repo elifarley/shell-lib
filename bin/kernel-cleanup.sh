d67b13f682479f17bb9601a6df3edd82783e8d5a
Elifarley C <519940+elifarley@users.noreply.github.com>
2023-09-05 08:47:56 +0200

    Update kernel-cleanup.sh

--- bin/kernel-cleanup.sh
+++ bin/kernel-cleanup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+du -hs /boot/*-generic
+echo 'sudo rm -v /boot/{vmlinuz,System.map,initrd.img,config}-{5.15.0-82,5.19.0-46,5.19.0-50}-generic'
+echo 'cp /boot/initrd.img-6.2.0-31-generic ~ecc/tmp'
+echo 'rm /boot/initrd.img-6.2.0-31-generic'
+exit
 
 # List all installed kernel versions
 kernels=$(ls /boot | grep -E '(vmlinuz|initrd|System.map)-[0-9]+\.[0-9]+\.[0-9]+-[0-9]+-generic' | sed -r 's/.*-([0-9]+\.[0-9]+\.[0-9]+-[0-9]+)-generic/\1/' | sort -V)
@@ -12,4 +17,3 @@ for kernel in $kernels_to_remove; do
     #echo sudo rm -f /boot/*-$kernel-generic
     du -hs /boot/*-$kernel-generic
 done
-
