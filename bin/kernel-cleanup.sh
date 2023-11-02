c9006b6433825dcd03338793eb716c19a1457bd4
Elifarley C <519940+elifarley@users.noreply.github.com>
2023-11-02 13:12:39 +0100

    Update kernel-cleanup.sh

--- bin/kernel-cleanup.sh
+++ bin/kernel-cleanup.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
+
 du -hs /boot/*-generic
-echo 'sudo rm -v /boot/{vmlinuz,System.map,initrd.img,config}-{5.15.0-82,5.19.0-46,5.19.0-50}-generic'
-echo 'cp /boot/initrd.img-6.2.0-31-generic ~ecc/tmp'
-echo 'rm /boot/initrd.img-6.2.0-31-generic'
-exit
 
 # List all installed kernel versions
-kernels=$(ls /boot | grep -E '(vmlinuz|initrd|System.map)-[0-9]+\.[0-9]+\.[0-9]+-[0-9]+-generic' | sed -r 's/.*-([0-9]+\.[0-9]+\.[0-9]+-[0-9]+)-generic/\1/' | sort -V)
+current_kernel="$(uname -r)"
+all_kernels=$(
+  du -hs /boot/*-generic | xargs -I {} basename {} | \
+  grep -Eo '[0-9]+[0-9.]+-[0-9]+' | sort | uniq | sort -V
+)
+older_kernels=$(
+  echo "$all_kernels" | \
+  grep -v "${current_kernel%-*}"
+)
+cat <<EOF
 
-# Keep only the latest kernel version
-kernels_to_remove=$(echo "$kernels" | head -n -1)
+Kernels:
+$older_kernels
+$current_kernel *** current kernel ***
 
-# Remove old kernel versions
-for kernel in $kernels_to_remove; do
-    #echo "Removing kernel version $kernel"
-    #echo sudo rm -f /boot/*-$kernel-generic
-    du -hs /boot/*-$kernel-generic
+EOF
+
+mkdir -p /var/tmp/current-kernel
+echo cp -v /boot/*$current_kernel* /var/tmp/current-kernel
+
+# Calculate the limit for the iteration
+((counter = $(echo "$older_kernels" | grep -c .) - 1))
+# Remove old kernel versions but 1, and also keep the current one.
+echo "$older_kernels" | while read -r kernel; do
+  #printf "\n# Removing kernel version %s\n" "$kernel"
+  #du -hs /boot/*-$kernel-generic
+  test "$kernel" || { echo skip ; continue ;}
+  echo "sudo rm -f /boot/*-$kernel-generic"
+  ((--counter)) || break
 done
+
+exit
