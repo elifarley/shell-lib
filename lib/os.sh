9d6fe429b67532b1869af6392bad57fb41ab94d0
Elifarley C <519940+elifarley@users.noreply.github.com>
2023-12-02 15:06:35 +0100

    Update os.sh

--- lib/os.sh
+++ lib/os.sh
@@ -1,4 +1,5 @@
 os_stats() {
   printf 'Memory usage: %s\n' $(cat /sys/fs/cgroup/memory/memory.usage_in_bytes | bytes2human)
+  cat /sys/fs/cgroup/cpu.pressure
   printf 'Memory usage: %s\n' $(cat /sys/fs/cgroup/cpu/cpuacct.usage)
 }
