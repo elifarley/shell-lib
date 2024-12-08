798595f729b7a707136c18ab7981a053054e1ef1
Elifarley C. <519940+elifarley@users.noreply.github.com>
2024-12-08 19:51:33 +0000

    Update template.sh

--- template/scripts/template.sh
+++ template/scripts/template.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-test "$-" && echo "Not meant to be sourced" && exit # if called via '.'
+test "$-" && echo "Not meant to be sourced" && exit 1 # if called via '.'
 CMD_BASE="$(readlink -f "$0" 2>/dev/null || greadlink -f "$0")" || CMD_BASE="$0"; CMD_BASE="$(dirname "$CMD_BASE")"
 
 # See (maybe) https://stackoverflow.com/a/23011530/299109
